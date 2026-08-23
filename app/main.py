4dc8251a91a3c1af0596d00ecffa1b455284cbbb
CemXokenc <mohikancem@gmail.com>
2026-08-24 02:59:39 +0300

    Solution Fix

--- app/main.py
+++ app/main.py
@@ -14,12 +14,27 @@ def move_file(command: str) -> None:
     if file_new.endswith("/"):
         file_new = os.path.join(file_new, os.path.basename(file_old))
 
-    directory = os.path.dirname(file_new)
+    path = ""
 
-    if directory:
-        os.makedirs(directory, exist_ok=True)
+    for dir_name in file_new.split("/")[:-1]:
+        path = os.path.join(path, dir_name)
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            pass
 
-    with open(file_old, "r") as old_file, open(file_new, "w") as new_file:
+    with (
+        open(os.path.join(
+            os.path.dirname(file_old),
+            os.path.basename(file_old)),
+            "r"
+        ) as old_file,
+        open(os.path.join(
+            path,
+            os.path.basename(file_new)),
+            "w"
+        ) as new_file
+    ):
         new_file.write(old_file.read())
 
     os.remove(file_old)
